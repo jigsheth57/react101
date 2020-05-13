4cf9fa4b7874425e429a7d0c505ddd53e35f0c12
Jig Sheth <jsheth@pivotal.io>
2020-05-13 08:22:19 -0500

    update

--- simple-calc/src/components/App.js
+++ simple-calc/src/components/App.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import Calculator from './Calculator';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    'display': 'flex',
+    'flexWrap': 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(50),
+      height: theme.spacing(30),
+    },
+  },
+}));
 
 export default function App({ initialData }) {
+  const classes = useStyles();
   const [total, setTotal] = useState(0);
   const updateTotal = (runningTotal) => {
     setTotal(runningTotal);
   };
   return (
-    <>
-      <div className="header">{initialData.appName}</div>
-      <div className="result">Total: {total}</div>
-      <Calculator onSubmit={updateTotal} />
-    </>
+    <div className={classes.root}>
+      <Paper elevation={3}>
+        <div className="header">{initialData.appName}</div>
+        <div className="result">Total: {total}</div>
+        <Calculator onSubmit={updateTotal} />
+      </Paper>
+    </div>
   );
 }
--- simple-calc/src/components/Calculator.js
+++ simple-calc/src/components/Calculator.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 export default function Calculator(props) {
   const [userInput, setUserInput] = useState('');
-  const [operation, setOperation] = useState('');
   const [runningTotal, setRunningTotal] = useState(0);
-
-  const handleSubmit = event => {
+  const classes = useStyles();
+  const handleSubmit = (event) => {
     event.preventDefault();
     let x = 0;
-    switch (operation) {
+    switch (event.currentTarget.value) {
       case '+':
         x = Number(runningTotal) + Number(userInput);
         setRunningTotal(x);
@@ -30,23 +47,71 @@ export default function Calculator(props) {
         setRunningTotal(x);
         props.onSubmit(x);
         break;
-      default: runningTotal;
+      default:
+        runningTotal;
     }
     setUserInput('');
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="number"
-        value={userInput}
-        onChange={event => setUserInput(event.target.value)}
-        placeholder=""
-        required
-      />
-      <button value='+' onClick={event => setOperation(event.target.value)}>+</button>
-      <button value='-' onClick={event => setOperation(event.target.value)}>-</button>
-      <button value='*' onClick={event => setOperation(event.target.value)}>*</button>
-      <button value='/' onClick={event => setOperation(event.target.value)}>/</button>
-    </form>
+    <Container maxWidth="sm">
+      <Typography
+        component="div"
+        style={{ backgroundColor: '#ebebeb' }}
+      >
+        <form className={classes.root} noValidate autoComplete="off">
+          <Input
+            type="number"
+            value={userInput}
+            onChange={(event) => setUserInput(event.currentTarget.value)}
+            placeholder=""
+            variant="filled"
+            color="primary"
+            inputProps={{ 'aria-label': 'description' }}
+            required
+          />
+          <br />
+          <Button
+            variant="contained"
+            size="medium"
+            color="primary"
+            className={classes.margin}
+            value="+"
+            onClick={(event) => handleSubmit(event)}
+          >
+            +
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            color="secondary"
+            className={classes.margin}
+            value="-"
+            onClick={(event) => handleSubmit(event)}
+          >
+            -
+          </Button>
+          <Button
+            variant="contained"
+            size="medium"
+            color="primary"
+            className={classes.margin}
+            value="*"
+            onClick={(event) => handleSubmit(event)}
+          >
+            *
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            color="secondary"
+            className={classes.margin}
+            value="/"
+            onClick={(event) => handleSubmit(event)}
+          >
+            /
+          </Button>
+        </form>
+      </Typography>
+    </Container>
   );
 }
